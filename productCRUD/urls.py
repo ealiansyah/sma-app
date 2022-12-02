0eac0b8611978adacd1147948738138fb58fe5f6
Rahadian Panji <rahadianpanjipanji@gmail.com>
2022-12-03 02:56:22 +0700

    Implement Delete product

--- productCRUD/urls.py
+++ productCRUD/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.get_product, name='Get Product'),
     path('create/', views.create_product, name='Create Product'),
     path('update/', views.update_product, name='Update Product'),
+    path('delete/', views.delete_product, name='Delete Product'),
 ]

7ec8950c6c4d0a980bcc46974071dbf79510d4a8
RahadianPanji <63046522+RahadianPanji@users.noreply.github.com>
2022-12-04 16:39:18 +0700

    [Feature/product crud] add Product_Category model (#5)
    * setup static file storage
    
    * create new app ProductCRUD and Product model
    
    * implement create product
    
    * implement Read Product
    
    * remove create table query when insert product
    
    * implement Update Product
    
    * Implement Delete product
    
    * use require_http_method decorator instead of using if
    
    * add price column to product
    
    * add category to product table
    
    * change category to string
    
    * add Product_Category model

--- productCRUD/models.py
+++ productCRUD/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+class Product_Category(models.Model):
+    nama_kategori = models.CharField(primary_key=True, max_length=50)
+    deskripsi = models.TextField()
+
+    def __str__(self):
+        return self.nama_kategori
+
 class Product(models.Model):
     name = models.CharField(max_length=63)
     description = models.CharField(max_length=127)
     quantity = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
     barcode_id = models.CharField(primary_key=True, max_length=127)
-    # TODO: Product_Category as foreign key
+    category = models.ForeignKey(Product_Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- productCRUD/views.py
+++ productCRUD/views.py
@@ -3,84 +3,84 @@ from django.shortcuts import render
 from django.http import HttpResponseBadRequest, HttpResponseNotAllowed, HttpResponse
 from django.db import connection
 from django.views.decorators.csrf import csrf_exempt
-
+from django.views.decorators.http import require_http_methods
 
 @csrf_exempt
+@require_http_methods(["POST"])
 def create_product(request):
-    if(request.method == 'POST'):
-        request_body = json.loads(request.body)
-        name = request_body.get("name")
-        description = request_body.get("description", "")
-        quantity = request_body.get("quantity", 0)
-        barcode_id = request_body.get("barcode_id")
+    request_body = json.loads(request.body)
+    name = request_body.get("name")
+    description = request_body.get("description", "")
+    quantity = request_body.get("quantity", 0)
+    barcode_id = request_body.get("barcode_id")
+    price = request_body.get("price", 0)
+    category = request_body.get("category")
 
-        if barcode_id == None  or  name == None:
-            return HttpResponseBadRequest("Barcode id and name can't be null")
+    if barcode_id == None  or  name == None  or  category == None:
+        return HttpResponseBadRequest("Barcode id, name, and category can't be null")
 
-        create_product_query = f"INSERT INTO product(name, description, quantity, barcode_id) \
-            VALUES('{name}', '{description}', {quantity}, '{barcode_id}');"
-        execute_query(create_product_query)
+    create_product_query = f"INSERT INTO product(name, description, quantity, barcode_id, price, category) \
+        VALUES('{name}', '{description}', {quantity}, '{barcode_id}', {price}, '{category}');"
+    execute_query(create_product_query)
 
-        return HttpResponse(status=201)
-
-    return HttpResponseNotAllowed(permitted_methods=['POST'])
+    return HttpResponse(status=201)
 
 @csrf_exempt
+@require_http_methods(["GET"])
 def get_product(request):
-    if(request.method == 'GET'):
-        name = request.GET.get('name', '')
+    name = request.GET.get('name', '')
 
-        get_product_query = f"SELECT * FROM product WHERE name LIKE '%{name}%';"
-        products = execute_and_fetchall_query(get_product_query)
-        products = json.dumps(products)
+    get_product_query = f"SELECT * FROM product WHERE name LIKE '%{name}%';"
+    products = execute_and_fetchall_query(get_product_query)
+    products = json.dumps(products)
 
-        return HttpResponse(products)
-
-    return HttpResponseNotAllowed(permitted_methods=['GET'])
+    return HttpResponse(products)
 
 @csrf_exempt
+@require_http_methods(["POST"])
 def update_product(request):
-    if(request.method == 'POST'):
-        request_body = json.loads(request.body)
-        name = request_body.get("name")
-        description = request_body.get("description")
-        quantity = request_body.get("quantity")
-        barcode_id = request_body.get("barcode_id")
+    request_body = json.loads(request.body)
+    name = request_body.get("name")
+    description = request_body.get("description")
+    quantity = request_body.get("quantity")
+    barcode_id = request_body.get("barcode_id")
+    price = request_body.get("price")
+    category = request_body.get("category")
 
-        if barcode_id == None:
-            return HttpResponseBadRequest("Barcode id can't be null")
+    if barcode_id == None:
+        return HttpResponseBadRequest("Barcode id can't be null")
 
-        update_product_query = "UPDATE product "
-        if name != None:
-            update_product_query += f"SET name = '{name}', "
-        if description != None:
-            update_product_query += f"SET description = '{description}', "
-        if quantity != None:
-            update_product_query += f"SET quantity = {quantity}, "
+    update_product_query = "UPDATE product "
+    if name != None:
+        update_product_query += f"SET name = '{name}', "
+    if description != None:
+        update_product_query += f"SET description = '{description}', "
+    if quantity != None:
+        update_product_query += f"SET quantity = {quantity}, "
+    if price != None:
+        update_product_query += f"SET price = {price}, "
+    if category != None:
+        update_product_query += f"SET category = '{category}', "
 
-        update_product_query = update_product_query[:-2]
-        update_product_query += f" WHERE barcode_id = '{barcode_id}';"
-        execute_query(update_product_query)
+    update_product_query = update_product_query[:-2]
+    update_product_query += f" WHERE barcode_id = '{barcode_id}';"
+    execute_query(update_product_query)
 
-        return HttpResponse(status=200)
-
-    return HttpResponseNotAllowed(permitted_methods=['POST'])
+    return HttpResponse(status=200)
 
 @csrf_exempt
+@require_http_methods(["POST"])
 def delete_product(request):
-    if(request.method == 'POST'):
-        request_body = json.loads(request.body)
-        barcode_id = request_body.get("barcode_id")
+    request_body = json.loads(request.body)
+    barcode_id = request_body.get("barcode_id")
 
-        if barcode_id == None:
-            return HttpResponseBadRequest("Barcode id can't be null")
+    if barcode_id == None:
+        return HttpResponseBadRequest("Barcode id can't be null")
 
-        delete_product_query = f"DELETE FROM product WHERE barcode_id = '{barcode_id}';"
-        execute_query(delete_product_query)
+    delete_product_query = f"DELETE FROM product WHERE barcode_id = '{barcode_id}';"
+    execute_query(delete_product_query)
 
-        return HttpResponse(status=200)
-
-    return HttpResponseNotAllowed(permitted_methods=['POST'])
+    return HttpResponse(status=200)
 
 
 def execute_query(query):

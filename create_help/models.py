7d42ef745ae6432fead84b83df9ea8383863252b
Mikhael <midebar@gmail.com>
2022-12-04 21:07:21 +0700

    Barebone v2

--- create_help/models.py
+++ create_help/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.db.models import CharField
 import uuid
 
-# Create your models here.
 
 class HelpTicket(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
--- create_help/views.py
+++ create_help/views.py
@@ -1,3 +1,24 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest, HttpResponseNotAllowed, HttpResponse
+from django.db import connection
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+@csrf_exempt
+@require_http_methods(["POST"])
+def create_help(request):
+    request_body = json.loads(request.body)
+    id = request_body.get('id')
+    title = request_body.get('title')
+    description = request_body.get('description')
+
+    if title == None or description == None:
+        return HttpResponseBadRequest("Title and Description must be filled")
+
+    create_help_query = f"INSERT INTO help_ticket(id, title, description) \
+        VALUES('{id}', '{title}', {description});"
+    with connection.cursor as cursor:
+        cursor.execute(create_help_query)
+
+    return HttpResponse(status=201)

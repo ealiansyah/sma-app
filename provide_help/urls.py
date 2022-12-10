574060b34daa337877fbce0b1c4e794cee707ddc
Mikhael <midebar@gmail.com>
2022-12-10 23:32:03 +0700

    Simplified provide help url

--- provide_help/urls.py
+++ provide_help/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
 urlpatterns = [
-    path('create/', views.provide_help, name='provide_help'),
+    path('', views.provide_help, name='provide_help'),
     path('view_list/', views.get_help_list, name='list_help'),
 ]
